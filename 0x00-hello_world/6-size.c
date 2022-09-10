35718a7481b5f638525e827eaa48b057534ae6ba
Innosaint2 <innocentonyebuenyi@gmail.com>
2022-09-10 16:19:16 -0700

    First experience with c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,13 @@ int main(void)
 	char c;
 	int i;
 	long int l;
-	long long int L;
+	long long int b;
 	float f;
 
 	printf("The size of a char is: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("The size of an int is: %lu byte(s)\n", (unsigned long)sizeof(i));
 			printf("The size of a long int is: %lu byte(s)\n", (unsigned long)sizeof(l));
-			printf("The size of a long long int is: %lu byte(s)\n", (unsigned long)sizeof(L));
+			printf("The size of a long long int is: %lu byte(s)\n", (unsigned long)sizeof(b));
 			printf("The size of a float is: %lu byte(s)\n", (unsigned long)sizeof(f));
 			return (0);
 }
