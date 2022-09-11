13b75d30008edc3fd2b69afddeadb9439784ba4d
Innosaint2 <innocentonyebuenyi@gmail.com>
2022-09-11 03:04:50 -0700

    First experience with c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	char c;
-	int i;
-	long int l;
-	long long int b;
-	float f;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-	printf("The size of a char is: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("The size of an int is: %lu byte(s)\n", (unsigned long)sizeof(i));
-			printf("The size of a long int is: %lu byte(s)\n", (unsigned long)sizeof(l));
-			printf("The size of a long long int is: %lu byte(s)\n", (unsigned long)sizeof(b));
-			printf("The size of a float is: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("The size of a char is: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("The size of an int is: %lu byte(s)\n", (unsigned long)sizeof(b));
+			printf("The size of a long int is: %lu byte(s)\n", (unsigned long)sizeof(c));
+			printf("The size of a long long int is: %lu byte(s)\n", (unsigned long)sizeof(d));
+			printf("The size of a float is: %lu byte(s)\n", (unsigned long)sizeof(e));
 			return (0);
 }
