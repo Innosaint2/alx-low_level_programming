b8fa8b989413b3ead221690a8c7ea1ca12638a97
Innosaint2 <innocentonyebuenyi@gmail.com>
2022-09-15 22:49:02 -0700

    Learning how to debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Tests the main function postive_or_negative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+	{
+		int i;
+
+		i = 0;
+		positive_or_negative(i);
+
+		return (0);
+	}
